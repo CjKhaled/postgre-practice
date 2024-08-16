1502f556374dc61ac6390ec1b56d858896fdc000
Cj Alexander <clauvinsalexander@gmail.com>
2024-08-15 21:31:31 -0400

    changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const userRouter = require('./routes/userRouter')
 
+require('dotenv').config()
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use('/', userRouter)
--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg')
 const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../gitignore/.env')})
 
 
 // a pool is a number of clients we hold connections to
